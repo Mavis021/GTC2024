10c40ec99e85cc32f7d4743efc2ea259a99aacc6
Mavis021 <077BCT043.mamata@pcampus.edu.np>
2023-12-29 23:43:39 +0545

    Assignment1

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
-import './App.css';
-//import ClassParent from './class';
-import ClassWork  from './classwork';
-//import Main from './main';
+// import './App.css';
+// //import ClassParent from './class';
+// import ClassWork  from './classwork';
+// //import Main from './main';
 
-function App(){
+// function App(){
 
    
+//   return (
+//     <div className="App">
+//       <h1 style={{color: "yellow"}}>But its Golden like Daylight</h1>
+//       <ClassWork />
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+import React from 'react';
+import Header from './Assignment1/Header';
+import Sidenav from './Assignment1/Sidenav';
+import MainContent from './Assignment1/MainContent';
+import Footer from './Assignment1/Footer';
+
+import './App.css';
+
+const App = () => {
   return (
-    <div className="App">
-      <h1 style={{color: "yellow"}}>But its Golden like Daylight</h1>
-      <ClassWork />
+    <div>
+      <Header title="Header"/>
+      <div className = "App-container">
+        <Sidenav title= "Sidenav"/>
+        <MainContent title = "Main Content"/>
+      </div>
+      <Footer title="Footer"/>
     </div>
   );
-}
-
+};
 
 export default App;
---
+++ Assignment1/Sidenav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidenav = ({title}) => {
+    return (
+        <div className="sidenav">
+        <h2>{title}</h2>
+        
+        </div>
+    );
+};
+
+export default Sidenav;
