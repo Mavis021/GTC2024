94e1dde9d75971027aadea85b859c477b468e478
Mavis021 <077BCT043.mamata@pcampus.edu.np>
2023-12-30 19:28:55 +0545

    Day2

---
+++ Day2/UseStateWithObject.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const UseStateWithObject = () => {
+  const [name, setName] = useState({ firstName: "", lastName: "" , age:21, address: ""});
+  return (
+    <form>
+      <input
+        type={"text"}
+        value={name.firstName}
+        onChange={(e) => setName({...name,firstName: e.target.value })}         //first import all the initial values of name then only update the first name
+      />
+      <input
+        type={"text"}
+        value={name.lastName}
+        onChange={(e) => setName({...name, lastName: e.target.value })}
+      />
+      <input
+        type={"number"}
+        value={name.age}
+        onChange={(e) => setName({...name, age: e.target.value })}
+      />
+      
+      <h2>Your first name is: {name.firstName}</h2>
+
+      <h2>Your last name is: {name.lastName}</h2>
+
+      <h2>Your age is: {name.age}</h2>
+    </form>
+  );
+};
+
+UseStateWithObject.propTypes = {};
+
+export default UseStateWithObject;
